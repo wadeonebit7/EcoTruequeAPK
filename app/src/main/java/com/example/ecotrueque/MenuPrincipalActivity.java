f56def934c5958ef555cf1666a4562c4a268085c
Wade <cuwainfo73@gmail.com>
2026-09-04 22:35:18 -0400

    V1 Version Three Interfaces (Only Views)

--- app/src/main/java/com/example/ecotrueque/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/ecotrueque/MenuPrincipalActivity.java
@@ -1,6 +1,9 @@
 package com.example.ecotrueque;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,22 @@ public class MenuPrincipalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_menu_principal);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MenuPrincipalActivity.this, PublicarObjetoActivity.class);
+            startActivity(intent);
         });
     }
 }
